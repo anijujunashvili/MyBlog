537c56864abafb803c1485b5ef2cf62472b3b8a4
Ani Jujunashvili <ana.jujunashvili@gmail.com>
2024-11-21 03:27:47 +0400

    change login function

--- src/supabase/auth/index.ts
+++ src/supabase/auth/index.ts
@@ -10,19 +10,12 @@ export const register = ({
   return supabase.auth.signUp({ email, password });
 };
 
-export const login = async ({
+export const login = ({
   email,
   password,
 }: {
   email: string;
   password: string;
 }) => {
-  try {
-    const res = await supabase.auth.signInWithPassword({ email, password });
-    console.log(res.data);
-    return res.data;
-  } catch (error) {
-    console.log("erroria");
-    return error;
-  }
+  return supabase.auth.signInWithPassword({ email, password });
 };
