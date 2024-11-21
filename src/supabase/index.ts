a409f9f39c6d3aaf009e725f305030bde49d567e
Ani Jujunashvili <ana.jujunashvili@gmail.com>
2024-11-21 11:22:54 +0400

    new change

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -6,5 +6,3 @@ export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
-
-//npx supabase gen types --lang=typescript --project-id nlmpmehbboluzlwlhwma --schema public > src/supabase/supabase.types.ts
