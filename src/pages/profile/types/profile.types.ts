52b951148e866d77cd504802bf8f0ed31b2be559
Ani Jujunashvili <ana.jujunashvili@gmail.com>
2024-12-12 02:41:43 +0400

    change type

--- src/pages/profile/types/profile.types.ts
+++ src/pages/profile/types/profile.types.ts
@@ -1,4 +1,4 @@
 import { Database } from "@/supabase/supabase.types";
 
 export type profileRow = Database["public"]["Tables"]["profiles"]["Row"];
-export type profilePayloadtype = Omit<profileRow, "id" | "updated_at">;
+export type profilePayloadtype = Omit<profileRow, "id">;
